8a9b15dc69cb93d1a41b032e4b988d5965af6b65
Gautam Anil <gautamanil20july@gmail.com>
2020-02-11 14:29:55 +0530

    Update Question completed

--- QAapp/QA/urls.py
+++ QAapp/QA/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('signup/' , views.signUP , name = 'signup') ,
     path('accounts/' , include('django.contrib.auth.urls')) ,
     path('addquestion/' , views.addQuestion , name = 'addQuestion') ,
+    path('restricted/' , views.unauthorisedAccess , name = 'restrict') ,
+    path('update/<int:question_id>' , views.updateQuestion , name = 'update') ,
 ]
--- QAapp/QA/views.py
+++ QAapp/QA/views.py
@@ -3,12 +3,17 @@ from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from .questionform import CreateQuestion
+from django.contrib.admin.views.decorators import staff_member_required
+from .models import Question
 
 # Create your views here.
 
 
 def home(request) :
-        return render(request , 'index.html')
+        questions = Question.objects.all()
+        return render(request , 'index.html' , {
+            'questions' : questions
+        })
 
 
 def signUP(request) :
@@ -26,16 +31,43 @@ def signUP(request) :
         'generateForm' : forms
     })
 
+@staff_member_required(login_url='restrict')
 def addQuestion(request) :
     if request.method == 'POST' :
         add = CreateQuestion(request.POST)
         if add.is_valid() :
             add.save()
-            return HttpResponse("Question added successfully")
+            return render(request , 'success.html')
 
     else :
         add = CreateQuestion()
 
     return render(request , 'QuestionForm.html' , {
         'renderForm' : add
+    })
+
+def unauthorisedAccess(request) :
+    return render(request , 'alert.html')
+
+
+@staff_member_required(login_url = 'restrict')
+def updateQuestion(request , question_id) :
+    question_id = int(question_id)
+
+    try :
+        question_selected = Question.objects.get(id = question_id)
+    except Question.DoesNotExist:
+        return redirect('home')
+
+    UpdateQuestion = CreateQuestion(request.POST or None, instance = question_selected)
+
+    if UpdateQuestion.is_valid():
+        UpdateQuestion.save()
+        return render(request , 'updatesuccess.html')
+
+    else :
+        UpdateQuestion = CreateQuestion(request.POST or None, instance = question_selected)
+
+    return render(request , 'QuestionForm.html' , {
+        'renderForm' : UpdateQuestion
     })
